9f9761f2a420c87040ed41d149c183ab79efb469
Victor Mesquini <mesquini@LIVE.COM>
2020-01-28 14:33:23 -0300

    new feature search pokemon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
-
 import api from "./server/index";
-
 import logo from "./img/logo.png";
 import LoadType from "./components/type";
-
 import "./global.css";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [q, setQ] = useState([]);
 
   useEffect(() => {
     async function loadPokemons() {
@@ -18,11 +16,22 @@ function App() {
     loadPokemons();
   }, []);
 
+  const filteredPokemons = () =>
+    q ? pokemons.filter(p => p.name.indexOf(q) > -1) : pokemons;
+
   return (
     <div className="main-conteiner">
       <img src={logo} alt="logo" />
+      <input
+        value={q}
+        onChange={e =>
+          setQ(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))
+        }
+        autoCapitalize
+        placeholder="Enter the name of the pokemon"
+      />
       <ul>
-        {pokemons.map(p => (
+        {filteredPokemons().map(p => (
           <li key={p.id}>
             <img className="pokemon" src={p.img} alt="pokemon" />
             <footer>
@@ -37,5 +46,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
